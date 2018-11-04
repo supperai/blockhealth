c06ec0cea82569d8154fcb6c7817e19b049b25b4
jianghaibo <jhb19940603@163.com>
2018-11-04 15:53:54 +0800

    部署智能合约

--- bhreact/src/ehr/queryehr/QueryEhrActions.js
+++ bhreact/src/ehr/queryehr/QueryEhrActions.js
@@ -1,17 +1,41 @@
 import request from '../../util/httpRequest';
 import store from "../../store";
+import ListRequestContract from '../../../build/contracts/ListRequest.json'
 
-// const contract = require('truffle-contract')
+const contract = require('truffle-contract')
 
 export function queryEhr(param) {
 
-    let web3 = store.getState().web3.web3Instance
+    let web3 = store.getState().web3.web3Instance;
 
     // Double-check web3's status.
     //todo
     if (typeof web3 !== 'undefined') {
 
         return function(dispatch) {
+            const listRequest = contract(ListRequestContract);
+            listRequest.setProvider(web3.currentProvider);
+            var listRequestInstance;
+
+            // Get current ethereum wallet.
+            web3.eth.getCoinbase((error, coinbase) => {
+                // Log errors, if any.
+                if (error) {
+                    console.error(error);
+                }
+
+                listRequest.deployed().then(function(instance) {
+                    listRequestInstance = instance;
+                    listRequestInstance.getHpstFromDss(param.diseaseName)
+                        .then(function(result) {
+                            var addressList = web3.toUtf8(result).split(',');
+                            // addressList
+
+                        })
+
+
+                })
+            })
 
         }
     } else {
