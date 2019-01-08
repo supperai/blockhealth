2c5c131d0102773da0a1a8173d63385257896575
jianghaibo <jhb19940603@163.com>
2019-01-08 16:21:38 +0800

    增加页面

---
+++ bhreact/src/ehr/datamining/DataMiningContainer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+export class DataMiningContainer extends Component {
+
+    render() {
+        return (
+            <div className="form">
+
+            </div>
+        );
+    }
+
+}
